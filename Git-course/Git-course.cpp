cfb4b6cb2519c63bf0f0e2cd7898098ebd286e55
A13handro <alexsandr.07k@gmail.com>
2025-10-18 21:14:17 +0300

    Я сделал функцию хода, в которой идет проверка на корректность ввода и совершение хода с отображением поля.

--- Git-course/Git-course.cpp
+++ Git-course/Git-course.cpp
@@ -1,6 +1,30 @@
 ﻿#include <iostream>
 
 int main(){
+    setlocale(LC_ALL, "RU");
+
+    //Поле 
+    char position[]{ '_', '_', '_', '_', '_', '_', '_', '_', '_', '_'};
+    
+    pole(0, 0, position);
+
+    for (int i = 0; i < 9; i++) {
+        int a, b;
+        if (i % 2 == 0){
+            pole(0, a * b, position);
+        cout << "Ход ноликов: строка: ";
+        cin >> a;
+        cout << "Ход ноликов: столбец: ";
+        cin >> b;
+    }
+        else {
+            pole(a * b, 0, position);
+            cout << "Ход крестиков: строка: ";
+            cin >> a;
+            cout << "Ход крестиков: столбец: ";
+            cin >> b;
+        }
+    }
 
     std::cout << "Hello World!\n";
 
